f4ed9f0f139df43116ccdc6a2edb97ac843993b5
hemanthbugata <hemanthbugata@gmail.com>
2025-05-03 19:44:34 +0530

    code

--- backend/models/product.js
+++ backend/models/product.js
@@ -1,22 +1,22 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
+    Id: {
+        type: Number,
+        required: true,
+        unique: true,
+        match: /^[0-9]{3}$/, // Ensures the productId is exactly 3 digits
+    },
     name: String,
     description: String,
     price: Number,
     image: String,
     category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category' },
-    brand: String,
     stock: Number,
     rating: {
         type: Number,
         default: 0,
-    },
-    images: [String],
-    rating: {
-        type: Number,
-        default: 0,
-    },
+    },    
     reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Review' }]
 }, { timestamps: true });
 
