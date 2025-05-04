c7eac1e81edf23b948fafb9d5c56b4359acd094e
hemanthbugata <hemanthbugata@gmail.com>
2025-05-04 13:36:01 +0530

    code

---
+++ backend/models/counter.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const counterSchema = new mongoose.Schema({
+  _id: { type: String, required: true }, // Name of the counter (e.g., 'productId')
+  seq: { type: Number, required: true }, // Current sequence value
+});
+
+module.exports = mongoose.model('Counter', counterSchema);
